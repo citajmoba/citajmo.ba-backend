7aa8c4e12d4283a8564cb36473078f5e4fc8f0da
Code Cademy <amercoding@gmail.com>
2021-12-16 10:12:00 -0800

    multiple CORS options fix2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(whitelist);
 
 const corsOptions = {
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))
